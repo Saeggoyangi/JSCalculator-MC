26b9c5833b786d3b36f16e2e837337ac57678cac
Saeggoyangi <115661254+Saeggoyangi@users.noreply.github.com>
2022-12-12 14:10:47 -0500

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+//knav
+//keyword, name, assignment, value
+let result = document.getElementById('result');
+
+
+//knapc
+function addNum(){
+    let firstNum = 2;
+    let secondNum = 2;
+    result.innerHTML = firstNum + secondNum;
+}
+
+
+function funcName(){
+    let firNum = 2;
+    let secNum = 2;
+    result.innerHTML = firNum - secNum;
+}
+
+function numNum(){
+    let firNum = 1;
+    let secNum = 2;
+    result.innerHTML = firNum - secNum;
+}
